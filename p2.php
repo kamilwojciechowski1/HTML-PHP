74244cf1345693910e96c266bfd422d539075812
kamilwojciechowski1 <wojciechowski.kamil@learnit.how>
2018-11-21 20:34:57 +0100

    TASK 004: Zaimplementowanie tworzenia trojkatow, naprawa pol i pustych wartosci

--- p2.php
+++ p2.php
@@ -7,19 +7,49 @@
 	</head>
 	<body>
 		<h1> Pole trójkąta </h1>
+			
+		<form action="p2.php" method="POST">
 		
-		<label for="a">Podaj a:</label> <input type="number" id="a"></br>
+			<label for="a">Podaj a:</label> <input type="number" name="a"></br>
+			
+			<label for="b">Podaj b:</label> <input type="number" name="b"></br>
+			
+			<label for="c">Podaj c:</label> <input type="number" name="c"></br>
 		
-		<label for="b">Podaj b:</label> <input type="number" id="b"></br>
+			<input type="submit" value="Oblicz pole"></br>
+			
+		</form>	
+			
+			<div id="wyn" style="margin-top: 20px;">
 		
-		<label for="c">Podaj c:</label> <input type="number" id="c"></br>
-		
-		<input type="submit" value="Oblicz pole"></br>
-		
-		<div id="wyn" style="margin-top: 20px;">
 		
 			<?php
-			
+				
+				if(isset($_POST["a"]) AND isset($_POST["b"]) AND isset($_POST["c"]))
+				{
+				
+					$a=$_POST["a"];
+					$b=$_POST["b"];
+					$c=$_POST["c"];
+				
+				
+					if (($a+$b<=$c) OR ($c+$b<=$a) OR ($a+$c<=$b))
+						{
+							echo "Nie da się stworzyć z danych boków trójkata";
+						}
+					else
+						{
+						
+						$p=($a+$b+$c)/2;
+						$wyn=$p*($p-$a)*($p-$b)*($p-$c);
+						echo $wyn;
+					}
+				}
+				else
+				{
+					echo "Pola są puste";
+				}
+				
 			
 			?>
 		
